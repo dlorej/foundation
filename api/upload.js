0440fe95e3e3bb33a4b2fc81339b12036687254b
Jerold <jeroldlimjunpin@gmail.com>
2024-10-21 10:03:09 +0800

    api test 1

---
+++ api/upload.js
@@ -0,0 +1,13 @@
+import {sql} from "@vercel/postgres"
+
+export default async function function1(req,res){
+    try{        
+        const output = await sql`INSERT INTO buildings (builing, state) VALUES ([(10,10),(20,30),(10,20),(50,20)])`
+        return res.status(200).json({message:"ok"})
+    
+    }catch(error){
+        return res.status(500).json({message:"fail"})
+    }
+}
+
+console.log(function1())
