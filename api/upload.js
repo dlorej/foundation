43764ddc6d7636267980a533e435c93075210272
Jerold <jeroldlimjunpin@gmail.com>
2024-10-21 10:08:51 +0800

    change insert into query

--- api/upload.js
+++ api/upload.js
@@ -2,7 +2,7 @@ import {sql} from "@vercel/postgres"
 
 export default async function function1(req,res){
     try{        
-        const output = await sql`INSERT INTO buildings (builing, state) VALUES ([(10,10),(20,30),(10,20),(50,20)])`
+        const output = await sql`INSERT INTO buildings (builing, state) VALUES (((10,10),(20,30),(10,20),(50,20)),2)`
         return res.status(200).json({message:"ok"})
     
     }catch(error){
