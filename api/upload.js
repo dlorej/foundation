68bfa03ade4f9241f2db930c7ec442fa83c6f0e4
Jerold <jeroldlimjunpin@gmail.com>
2024-10-21 18:51:06 +0800

    api test with poly 3

--- api/upload.js
+++ api/upload.js
@@ -9,7 +9,7 @@ export default async function function1(req,res){
             const item2 = item.split("=")
             data_json[item2[0]] = item2[1]
         }
-        const poly = data_json["poly"]
+        const poly = decodeURIComponent(data_json["poly"])
         const state = data_json["state"]
         const output = await sql`INSERT INTO buildings (builing, state) VALUES (${poly},${state})`
         return res.status(200).json({message:"ok"})
