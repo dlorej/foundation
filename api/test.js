56c47b8863b2aa739b38555fc180a829f42009ca
Jerold <jeroldlimjunpin@gmail.com>
2024-12-12 22:26:20 +0800

    return to testing api

--- api/test.js
+++
@@ -1,6 +0,0 @@
-// var t = [[0,1],[0,2],[0,3]]
-// const poly = encodeURIComponent('((1,1),(2,3),(4,3),(6,9))')
-// t = encodeURIComponent(t)
-// console.log(t)
-// console.log(poly)
-console.log(null)
--- api/upload.js
+++ api/upload.js
@@ -1,20 +1,32 @@
 import {sql} from "@vercel/postgres"
 
-export default async function function1(req,res){
-    const data_json = {}
-    try{
-        const pattern = /(?<=[?]).*/
-        const dataset = String(pattern.exec(req.url)).split("&")
-        for (const item of dataset){
-            const item2 = item.split("=")
-            data_json[item2[0]] = item2[1]
-        }
-        const poly = decodeURIComponent(data_json["encoded_poly"])
-        const name = data_json["name"]
-        const output = await sql`INSERT INTO buildings (boundary, name) VALUES (${poly},${name})`
-        return res.status(200).json({message:"ok"})
+// export default async function function1(req,res){
+//     const data_json = {}
+//     try{
+//         const pattern = /(?<=[?]).*/
+//         const dataset = String(pattern.exec(req.url)).split("&")
+//         for (const item of dataset){
+//             const item2 = item.split("=")
+//             data_json[item2[0]] = item2[1]
+//         }
+//         const poly = decodeURIComponent(data_json["encoded_poly"])
+//         const name = data_json["name"]
+//         const output = await sql`INSERT INTO buildings (boundary, name) VALUES (${poly},${name})`
+//         return res.status(200).json({message:"ok"})
     
+//     }catch(error){
+//         return res.status(500).json({message:"fail"})
+//     }
+// }
+
+export default async function test(req,res){
+    try{
+        const output = await sql`INSERT INTO buildings (boundary, name) VALUES (a,b)`
+        return res.status(200).json({message:"ok"})
     }catch(error){
         return res.status(500).json({message:"fail"})
     }
+    
 }
+
+//write test case
