d769d47e2bc29aba4dc92db86358f29fb7659101
Jerold <jeroldlimjunpin@gmail.com>
2024-12-24 02:11:55 +0800

    minor changes to upload, getpolys from database to map works

--- api/upload.js
+++ api/upload.js
@@ -1,18 +1,22 @@
 import {sql} from "@vercel/postgres"
 
 export default async function function1(req,res){
+    var {name,poly} = req.body
+    if (name == null){
+        name = ""
+    }
     try{
-        const {name,poly} = req.body
-
         const check = await sql`SELECT * FROM buildings WHERE boundary = ${String(poly)}`
         if (check.rows.length == 0){
-            var output = await sql`INSERT INTO buildings (boundary, name, state) VALUES (${String(poly)},${name},0)`
+            await sql`INSERT INTO buildings (boundary, name, state) VALUES (${String(poly)},${name},0)`
+            var output = "success"
         } else { 
             var output = "exists"
         }
         return res.status(200).json({message:output})
     
     }catch(error){
+        console.log(name,poly)
         return res.status(500).json({message:"fail"})
     }
 }
