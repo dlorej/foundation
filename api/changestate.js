bc825652429fca80f2edc009ef1cba24e0e0a6d5
Jerold <jeroldlimjunpin@gmail.com>
2025-01-01 01:49:02 +0800

    added top bar, working on login

--- api/changestate.js
+++ api/changestate.js
@@ -7,11 +7,18 @@ export default async function function1(req,res){
         name = ""
     }
     try{
-        console.log(name,poly)
-
-        await sql`UPDATE buildings
-            SET state = -1
-            WHERE name = ${name} AND boundary = ${String(poly)}`
+        const query = await sql`SELECT state FROM buildings 
+            WHERE name = ${name} AND boundary = ${String(poly)} `
+        if (query.rows.length == 1){
+            if (query.rows[0]["state"] == 0){
+                var newstate = -1
+            }else if (query.rows[0]["state"] == -1){
+                var newstate = 0
+            }
+            await sql`UPDATE buildings
+                SET state = ${newstate}
+                WHERE name = ${name} AND boundary = ${String(poly)}`
+        }
         return res.status(200).json({message:"success"})
     }catch{
         return res.status(500).json({message:"fail"})
