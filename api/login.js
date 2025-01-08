1c37b87b6ef156a81569007f109a9521f799c037
Jerold <jeroldlimjunpin@gmail.com>
2025-01-09 00:42:41 +0800

    autocomplete email verification testing 2

--- api/login.js
+++ api/login.js
@@ -1,7 +1,8 @@
 import {sql} from "@vercel/postgres"
 
 export default async function function1(req,res){
-    var {email} = req.body
-    const check = await sql`SELECT * FROM buildings WHERE boundary = ${String(poly)}`
-        if (check.rows.length == 0){}
+    var {uid,email} = req.body
+    console.log(uid,email)
+    // const check = await sql`SELECT * FROM buildings WHERE boundary = ${String(poly)}`
+    //     if (check.rows.length == 0){}
 }
