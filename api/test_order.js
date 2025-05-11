a11471f096f289142ab38308a9d9bd64a3a66cfb
Jerold <jeroldlimjunpin@gmail.com>
2025-05-12 00:35:30 +0800

    testing get from testingorder table 4

--- api/test_order.js
+++ api/test_order.js
@@ -1,8 +1,11 @@
 import {sql} from "@vercel/postgres"
 
 export default async function function1(req,res){
-    var orderid = req.body
+    console.log(req)
+    var {orderid} = req.body
+    console.log(orderid)
     const check = await sql`SELECT items FROM testingorder WHERE orderid = ${orderid}`
+    console.log(check)
     if (check.rows.length == 1){
         return res.status(200).json({message:"success",content:check.rows[0]})   
     }else{
