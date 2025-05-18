cf0dc9b47637aed999b729065367a89e3f8f5860
Jerold <jeroldlimjunpin@gmail.com>
2025-05-19 02:40:35 +0800

    testing get from menu 3

--- api/menu_check.js
+++ api/menu_check.js
@@ -1,14 +1,10 @@
 import {sql} from "@vercel/postgres"
 
 export default async function function1(req,res){
-    var {item_serial} = req.body
-    const check = await sql`SELECT item_name FROM menu WHERE item_serial = ${item_serial}`
+    var {item_name} = req.body
+    const check = await sql`SELECT price FROM menu WHERE item_name = ${item_name}`
     if (check.rows.length == 1){
-        const price_check = await sql`SELECT price FROM menu WHERE item_serial = ${item_serial}`
-        return res.status(200).json({
-            item_name:check.rows[0],
-            item_price:price_check[0]
-        })   
+        return res.status(200).json(check.rows[0])   
     }else{
         return res.status(200).json({message:"fail"})
     }
